2c519664f4de9481c3202250d719ce9117691bf4
sheveioncallender-boop <sheveioncallender@gmail.com>
2026-09-02 18:18:49 -0400

    Add files via upload

---
+++ spx_reconciliation_overview/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "SPX Bank Reconciliation Overview",
+    "summary": "Peachtree-style bank reconciliation visibility using Odoo's native accounting flow",
+    "version": "19.0.1.0.2",
+    "category": "Accounting/Accounting",
+    "author": "Spxcorp Limited",
+    "website": "https://spxcorp.net",
+    "license": "LGPL-3",
+    "depends": [
+        "account",
+        "account_accountant",
+    ],
+    "data": [
+        "security/reconciliation_security.xml",
+        "security/ir.model.access.csv",
+        "views/reconciliation_overview_views.xml",
+        "reports/reconciliation_overview_report.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
